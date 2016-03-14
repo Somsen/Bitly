3c9d57e2d020e705b375ac3817eb0e3a896fcd7f
Somsen <pm.somsen@gmail.com>
2016-03-14 22:55:17 +0000

    Login working TODO: process incorrect inputs

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: login_params[:email])
+    password_hash = Digest::MD5.hexdigest(login_params[:password])
+
+    if password_hash == @user.password_digest
+      cookies.signed[:login_signed] = @user.name
+      redirect_to welcome_index_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    cookies.delete :login_signed
+    redirect_to welcome_index_path
+  end
+
+  private
+
+  def login_params
+    params.require(:session).permit(:email, :password)
+  end
+end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,7 @@
 module WelcomeHelper
+
+  def logged_in?
+    cookies.signed[:login_signed].nil?
+  end
+
 end
